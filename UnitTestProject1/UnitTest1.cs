a72999e6b3060f91d77bb3fca37553ab16143434
WerLaj <weronikalajewska@gmail.com>
2018-03-05 10:53:25 +0100

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void EmptyString()
+        {
+            string zerothString = "";
+            int expectedSum = 0;
+            Program prog = new Program();
+            int actualSum = prog.Add(zerothString);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void OneNumberString()
+        {
+            string oneString = "5";
+            int expectedSum = 5;
+            Program prog = new Program();
+            int actualSum = prog.Add(oneString);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void TwoNumberString()
+        {
+            string oneString = "5,4";
+            int expectedSum = 9;
+            Program prog = new Program();
+            int actualSum = prog.Add(oneString);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)]
+        public void MoreNumbersString()
+        {
+            string oneString = "5\n4,3,2,1";
+            int expectedSum = 15;
+            Program prog = new Program();
+            int actualSum = prog.Add(oneString);
+
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+    }
+}
